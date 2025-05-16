b13fd187a529cefc737242a91f87a082a1703993
Valentina Iramain <valentinairamain2020@gmail.com>
2025-05-16 17:11:59 -0300

    Se agrega archivo README.md

--- js/app.js
+++ js/app.js
@@ -3,15 +3,12 @@
 //agregar tarea con innerHTML
 function agregarTarea(e) {
     e.preventDefault(); //evitar que recargue la pagina
-    console.log('agregar tareas')
 
     //busca la tabla de tareas con la etiqueta tr
-    const tablaTareas = document.querySelector('tr')
-    console.log(tablaTareas)
+    const tablaTareas = document.querySelector('table')
 
     //seleccionar el texto que escribio en la tarea
     const tarea = document.querySelector('#tareaInput').value
-    console.log(tarea)
 
     tablaTareas.innerHTML += `<td class="col-10 justify-content-start align-item-center">${tarea}</td>
                               <td class="col-2 justify-content-center align-item-center"><button class="btn btn-danger"><i class="bi bi-trash3-fill"></i></button></td>`
@@ -26,5 +23,4 @@ function agregarTarea(e) {
 
 // lista tareas
 const formularioTareas = document.querySelector('#tareaForm')
-console.log(formularioTareas)
 formularioTareas.addEventListener('submit', agregarTarea)
