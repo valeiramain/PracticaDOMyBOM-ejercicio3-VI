72cfd9aa4758cf0c5fb8a8d40a472a3eaf1bc68f
Valentina Iramain <valentinairamain2020@gmail.com>
2025-05-20 17:09:40 -0300

    No se pudo completar funcion eliminar tarea

--- js/app.js
+++ js/app.js
@@ -4,26 +4,32 @@
 function agregarTarea(e) {
     e.preventDefault(); //evitar que recargue la pagina
 
-    //busca el tbody de la tabla de tareas
-    const tablaTareas = document.querySelector('.list-group')
-    console.log(tablaTareas)
+    //busca lista de la tabla de tareas
+    const listaTareas = document.querySelector('.list-group')
+    console.log(listaTareas)
 
     //seleccionar el texto que escribio en el formulario
     const tarea = document.querySelector('#tareaInput').value
     console.log('tarea:'+tarea)
 
-    tablaTareas.innerHTML += `<li class="list-group-item d-flex justify-content-between align-items-start">${tarea}<button class="btn btn-danger d-flex justify-content-between align-items-end" onclick('eliminarTarea()')><i class="bi bi-trash3-fill"></i></button></li>`
+    listaTareas.innerHTML += `<li class="list-group-item d-flex justify-content-between align-items-start">${tarea}
+                            <button class="btn btn-danger d-flex justify-content-between align-items-end" onclick="eliminarTarea()"><i class="bi bi-trash3-fill"></i></button></li>`
+    console.log(indiceLista)
+    indiceLista = indiceLista + 1
     // blanquer input
     formularioTareas.reset();
 }
 
+function eliminarTarea(){
+    console.log('en funcion eliminar: ')
+    const borrarTarea = document.getElementsByTagName('li')
+    borrarTarea.remove()
+}
+
 
 //-------------------eventos---------------------
-
-// lista tareas
+// formulario para ingresar tareas
 const formularioTareas = document.querySelector('#tareaForm')
 formularioTareas.addEventListener('submit', agregarTarea)
-
-
 
 
